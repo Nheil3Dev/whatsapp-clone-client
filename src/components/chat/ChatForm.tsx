89afd3dce8b85f70aeb53bbb91b07725fc8ca353
Nheil3Dev <98476350+Nheil3Dev@users.noreply.github.com>
2023-10-20 18:13:24 +0200

    emojis integration done

--- src/components/chat/ChatForm.tsx
+++ src/components/chat/ChatForm.tsx
@@ -1,19 +1,25 @@
-import EmojiPicker from 'emoji-picker-react'
+import EmojiPicker, { type EmojiClickData } from 'emoji-picker-react'
 import { useState } from 'react'
 import { EmojiIcon } from '../icons/EmojiIcon'
 import { PlusIcon } from '../icons/PlusIcon'
 import { SendIcon } from '../icons/SendIcon'
+import { XIcon } from '../icons/XIcon'
 import './ChatForm.css'
 
 export function ChatForm () {
   const [active, setActive] = useState(false)
+  const [message, setMessage] = useState<string>('')
+  const handleEmoji = (emojiObject: EmojiClickData) => {
+    setMessage(prevText => prevText + emojiObject.emoji)
+  }
   return (
     <footer>
       {active && (
         <span className="emoji-picker-container">
           <EmojiPicker
+            onEmojiClick={handleEmoji}
             width="100%"
-            height="400px"
+            height="320px"
             previewConfig={{ showPreview: false }}
           />
         </span>
@@ -21,18 +27,29 @@ export function ChatForm () {
       <form>
         <div className="icon-container">
           <button
-            className="icon-button"
+            className='icon-button'
+            type='button'
+            onClick={() => setActive(false)}
+          >
+            <XIcon />
+          </button>
+          <button
+            className={active ? 'icon-button active-emoji' : 'icon-button'}
             type="button"
-            onClick={() => setActive(!active)}
+            onClick={() => setActive(true)}
           >
             <EmojiIcon />
           </button>
-          <PlusIcon />
+          <button className='icon-button' type='button'>
+            <PlusIcon />
+          </button>
         </div>
         <input
           className="input-text"
           type="text"
           placeholder="Escribe un mensaje"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           autoComplete="off"
         />
         <button onClick={(e) => e.preventDefault()} className="icon-button" type="submit">
